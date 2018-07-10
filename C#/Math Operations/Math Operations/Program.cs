17f8a46651c9b2d15df4915868eee2f55a55102a
Stone Yan <stoneyan1995@gmail.com>
2018-07-10 16:19:36 -0700

    Math Operations

---
+++ C#/Math Operations/Math Operations/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Math_Operations
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Input a number.");
+            double num = Convert.ToDouble(Console.ReadLine());
+            double mult = num * 50;
+            Console.WriteLine("Your number times 50 is "+mult);
+            Console.WriteLine("Input another number.");
+            double numb = Convert.ToDouble(Console.ReadLine());
+            double add = numb + 25;
+            Console.WriteLine("Your number plus 25 is " + add);
+            Console.WriteLine("Input another number.");
+            double numbe = Convert.ToDouble(Console.ReadLine());
+            double divide = numbe / 12.5;
+            Console.WriteLine("Your number divided by 12.5 is " + divide);
+            Console.WriteLine("Input another number.");
+            double number = Convert.ToDouble(Console.ReadLine());
+            bool compare = number > 50;
+            Console.WriteLine(compare);
+            Console.WriteLine("Input another number.");
+            double n = Convert.ToDouble(Console.ReadLine());
+            double remainder = n % 7;
+            Console.WriteLine("The remainder of your number divided by 7 is " + remainder);
+
+
+
+
+        }
+    }
+}
