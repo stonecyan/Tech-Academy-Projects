42dca1ada9ef7dc2a0962aeee59b40221da0841b
Stone Yan <stoneyan1995@gmail.com>
2018-07-10 15:18:10 -0700

    Fixes

--- C#/Daily Report/Daily Report/Program.cs
+++ C#/Daily Report/Daily Report/Program.cs
@@ -11,18 +11,15 @@ namespace Daily_Report
             Console.WriteLine("What course are you on?");
             string course = Console.ReadLine();
             Console.WriteLine("What page number?");
-            string page = Console.ReadLine();
-            int pagenum = Convert.ToInt32(page);
+            int page = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Do you need help with anything? Please answer 'true' or 'false'.");
-            string help = Console.ReadLine();
-            bool Help = Convert.ToBoolean(help);
+            bool help = Convert.ToBoolean(Console.ReadLine());
             Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
             string positive = Console.ReadLine();
             Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
             string feedback = Console.ReadLine();
             Console.WriteLine("How many hours did you study today?");
-            string hours = Console.ReadLine();
-            int Hours = Convert.ToInt32(hours);
+            double hours = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
             Console.ReadLine();
         }
