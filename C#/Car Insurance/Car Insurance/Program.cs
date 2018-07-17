1599ef4cfe47c48d7fd400ee33a6e55bad4f515d
Stone Yan <stoneyan1995@gmail.com>
2018-07-17 16:25:27 -0700

    Car Insurance

---
+++ C#/Car Insurance/Car Insurance/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Insurance
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? Answer true or false");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Qualified for insurance?");
+            bool insurance = (age > 15 && dui == false && tickets <= 3);
+            Console.WriteLine(insurance);
+            Console.ReadLine();
+        }
+    }
+}
