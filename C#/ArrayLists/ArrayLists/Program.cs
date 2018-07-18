8768d44d3f9e902490125ae0986990aed393a916
Stone Yan <stoneyan1995@gmail.com>
2018-07-18 16:26:14 -0700

    ArrayLists

---
+++ C#/ArrayLists/ArrayLists/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array1 = new string[] { "My", "name", "is", "Stone", "Yan" };
+            int[] array2 = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Select an index of the string array from 0-5.");
+            int stringindex = Convert.ToInt32(Console.ReadLine());
+            if (stringindex>array1.Length)
+            {
+                Console.WriteLine("Selected index does not exist.");
+            }
+            else
+            {
+                Console.WriteLine(array1[stringindex]);
+            }
+            Console.WriteLine("Select another index of the integer array from 0-5.");
+            int intindex = Convert.ToInt32(Console.ReadLine());
+            if (intindex > array2.Length)
+            {
+                Console.WriteLine("Selected index does not exist.");
+            }
+            else
+            {
+                Console.WriteLine(array2[intindex]);
+            }
+            List<string> array3 = new List<string>();
+            array3.Add("Hello!");
+            array3.Add("My");
+            array3.Add("Name");
+            array3.Add("is");
+            array3.Add("Stone");
+
+            Console.WriteLine("Select another index of the string List from 0-4");
+            int listindex = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(array3[listindex]);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+    class String
+    {
+        static void Main(string[] args)
+        {
+        string string1 = "My name";
+        string string2 = " is";
+        string string3 = " Stone";
+        string combined = string1 + string2 + string3;
+        Console.WriteLine(combined);
+        string upper = string3.ToUpper();
+        Console.WriteLine(upper);
+
+        System.Text.StringBuilder sb = new System.Text.StringBuilder();
+    
+        sb.Append("It matters not how strait the gate, ");
+        sb.Append("It matters not how strait the gate, ");
+        sb.Append("I am the master of my fate, ");
+        sb.Append("I am the captain of my soul. ");
+        Console.WriteLine(sb);
+        Console.ReadLine();
+        }
+    }
