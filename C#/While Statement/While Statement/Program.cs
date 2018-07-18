49098e9dc41e882eebc2460093677354203ebd2c
Stone Yan <stoneyan1995@gmail.com>
2018-07-17 18:12:33 -0700

    While Loop

---
+++ C#/While Statement/While Statement/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_Statement
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Count to what number?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int n = 1;
+            while (n!=number+1)
+            {
+                Console.WriteLine(n);
+                n = n + 1;
+            }
+
+
+            Console.WriteLine("Count down from what number?");
+            int num = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                Console.WriteLine(num);
+                num = num - 1;
+            }
+            while (num > 0);
+            Console.ReadLine();
+        }
+    }
+}
