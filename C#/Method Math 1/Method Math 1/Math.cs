d5be84b51a8c02e2e2a54ec60384bc02cca1ffe9
Stone Yan <stoneyan1995@gmail.com>
2018-07-20 21:47:13 -0700

    Method Math 1

---
+++ C#/Method Math 1/Method Math 1/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Math_1
+{
+    class Math
+    {
+        public static int Add(int n,int m=0)
+        {
+            int Add = n + m;
+            return Add;
+        }       
+    }
+}
---
+++ C#/Method Math 1/Method Math 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Math_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer.");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter another integer. This is optional, so enter \"None\" if you do not want to enter an integer. ");
+            string n2 = Console.ReadLine();
+            if (n2 == "None")
+            {
+                Console.WriteLine(Math.Add(n1));
+            }
+            else
+            {
+                int n22 = Convert.ToInt32(n2);
+                Console.WriteLine(Math.Add(n1, n22));
+            }
+            Console.ReadLine();
+        }
+    }
+}
