afed68cf8ecc59cafa709baeb84c16a2c8e6f177
Stone Yan <stoneyan1995@gmail.com>
2018-07-22 16:00:19 -0700

    Method Math 2

---
+++ C#/Method Math 2/Method Math 2/Math.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Math_2
+{
+    class MathOp
+    {
+        public void Math(int n1,int n2)
+        {
+            int square = n1 * n1;
+            Console.WriteLine(n2);
+        }
+
+
+    }
+    
+}
---
+++ C#/Method Math 2/Method Math 2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Math_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOp mathop = new MathOp();
+            mathop.Math(4, 2);
+            mathop.Math(n1: 4, n2: 2);
+            Console.ReadLine();
+        }
+    }
+}
