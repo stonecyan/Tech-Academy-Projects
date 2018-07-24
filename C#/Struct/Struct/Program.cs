c4a9d092c156e18598d771f263e52ca028c0f73c
Stone Yan <stoneyan1995@gmail.com>
2018-07-24 14:44:11 -0700

    Struct

---
+++ C#/Struct/Struct/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number n = new Number();
+            n.Amount = 45.3;
+            Console.WriteLine(n.Amount);
+            Console.ReadLine();
+        }
+    }
+}
