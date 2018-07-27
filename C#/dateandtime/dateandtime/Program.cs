90bf79fc1415291d9efce7ed3161f4aa061a952a
Stone Yan <stoneyan1995@gmail.com>
2018-07-27 16:41:30 -0700

    dateandtime

---
+++ C#/dateandtime/dateandtime/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dateandtime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Enter a number.");
+            double n = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(DateTime.Now.AddHours(n));
+            Console.ReadLine();
+        }
+    }
+}
